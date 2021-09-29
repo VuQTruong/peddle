1fd492eab11b9f1f22c43bf4703945dcfda53796
Vu Truong <q_truong@fanshaweonline.ca>
2021-09-29 00:10:31 -0400

    connect to mongodb atlas

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,11 @@
 import { app } from './server';
 import connectToDB from './db/connect';
-import { DatabaseConnectionError } from './errors/database-connection-error';
 
 const host = (process.env.HOST || 'localhost') as string;
 const port = (process.env.PORT || 5000) as number;
 app.listen(port, host, () => {
-  // Connect to database
-  //   connectToDB();
   console.log(`Serve at http://${host}:${port}`);
 
+  // Connect to database
+  connectToDB();
 });
