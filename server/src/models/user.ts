b653db6ae394b5a63b42b4b2495e6ac53c88d665
Sion Young <sioniunn@gmail.com>
2021-10-03 08:40:22 -0400

    bugfix: file case

---
+++ server/src/models/user.ts
@@ -0,0 +1,107 @@
+import mongoose from "mongoose";
+import { hashPassword } from "../utilities/password-util";
+
+// an interface that describe the properties
+// that are required to create a new user
+interface UserAttrs {
+  firstName: string;
+  lastName: string;
+  photo: string;
+  email: string;
+  password: string;
+  lat: number;
+  lng: number;
+  postalCode: string;
+  isPremiumMember: boolean;
+  dislikedItemIds: string[];
+}
+
+// an interface that describe the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// an inteface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  firstName: string;
+  lastName: string;
+  photo: string;
+  email: string;
+  password: string;
+  lat: number;
+  lng: number;
+  postalCode: string;
+  isPremiumMember: boolean;
+  dislikedItemIds: string[];
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+    },
+    lat: {
+      type: Number,
+      required: true,
+    },
+    lng: {
+      type: Number,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    isPremiumMember: {
+      type: Boolean,
+      required: true,
+    },
+    dislikedItemIds: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await hashPassword(this.get("password"));
+    this.set("password", hashed);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
